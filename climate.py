7a552020968732b0de2b0eeb60e2f98bb2bfe98a
Demetrius Albuquerque <causbla@gmail.com>
2018-01-15 17:02:52 -0400

    added post to create new climates

--- climate.py
+++ climate.py
@@ -1,11 +1,31 @@
 from flask_restful import Resource
 from flask import jsonify
 from climatemodel import ClimateModel
+from climatemodel import db
 
 class Climate(Resource):
 
     def get(self, cli_id):
         if not cli_id:
-            return jsonify({'Error'})
+            self.error_handler('not valid')
         response = ClimateModel.query.filter_by(id=cli_id).first()
-        return jsonify(response.as_dict())
+        if response:
+            return jsonify(response.as_dict())
+        else:
+            self.error_handler('not found')
+
+    def delete(self, cli_id):
+        if not cli_id:
+            self.error_handler('not valid')
+
+        response = ClimateModel.query.filter_by(id=cli_id).first()
+        if response:
+            db.session.delete(response);
+            x = db.session.commit()
+            print x
+            return jsonify(str(x))
+        else:
+            self.error_handler('not found')
+
+    def error_handler(self, message):
+        return jsonify({'Error': message})
--- climatelist.py
+++ climatelist.py
@@ -1,7 +1,9 @@
 from flask_restful import Resource
 from flask import jsonify
-# from climatemodel import ClimateModel
+from flask import request
 from climatemodel import ClimateModel
+from climatemodel import db
+
 
 class ClimateList(Resource):
 
@@ -11,3 +13,26 @@ class ClimateList(Resource):
         for c in climates:
             response.append(c.as_dict())
         return jsonify(response)
+
+    def post(self):
+        if request.is_json:
+            content = request.get_json()
+            new = ClimateModel(db)
+            if content["temp_max"]:
+                new.temp_max = float(content["temp_max"])
+            if content["temp_min"]:
+                new.temp_min = float(content["temp_min"])
+            if content["rainfall"]:
+                new.rainfall = bool(content["rainfall"])
+            if content["date"]:
+                new.date = content["date"]
+            db.session.add(new)
+            db.session.commit()
+
+            return jsonify({"created": new.as_dict()})
+        else:
+            error_handler('problem to process')
+
+
+    def error_handler(message):
+        return jsonify({"fail": message})
